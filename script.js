2db99d7c17055dc133eb5c63b16095e350e739a2
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-02-02 02:40:09 -0700

    Numeric Separator

--- script.js
+++ script.js
@@ -335,28 +335,45 @@ btnSort.addEventListener("click", function (e) {
 
 //* The Remainder Operator *//
 
-console.log(5 % 2);
-console.log(5 / 2); // 5 = 2 * 2 + 1
+// console.log(5 % 2);
+// console.log(5 / 2); // 5 = 2 * 2 + 1
 
-console.log(8 % 3);
-console.log(8 / 3); // 8 = 2 * 3 + 2
+// console.log(8 % 3);
+// console.log(8 / 3); // 8 = 2 * 3 + 2
 
-console.log(6 % 2);
-console.log(6 / 2);
+// console.log(6 % 2);
+// console.log(6 / 2);
 
-console.log(7 % 2);
-console.log(7 / 2);
+// console.log(7 % 2);
+// console.log(7 / 2);
 
-const isEven = (n) => n % 2 === 0;
-console.log(isEven(8));
-console.log(isEven(23));
-console.log(isEven(514));
+// const isEven = (n) => n % 2 === 0;
+// console.log(isEven(8));
+// console.log(isEven(23));
+// console.log(isEven(514));
 
-labelBalance.addEventListener("click", function () {
-  [...document.querySelectorAll(".movements__row")].forEach(function (row, i) {
-    // 0, 2, 4, 6
-    if (i % 2 === 0) row.style.backgroundColor = "orangered";
-    // 0, 3, 6, 9
-    if (i % 3 === 0) row.style.backgroundColor = "blue";
-  });
-});
+// labelBalance.addEventListener("click", function () {
+//   [...document.querySelectorAll(".movements__row")].forEach(function (row, i) {
+//     // 0, 2, 4, 6
+//     if (i % 2 === 0) row.style.backgroundColor = "orangered";
+//     // 0, 3, 6, 9
+//     if (i % 3 === 0) row.style.backgroundColor = "blue";
+//   });
+// });
+
+//* Numeric Separators *//
+
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number("230_000"));
+console.log(parseInt("230_000"));
