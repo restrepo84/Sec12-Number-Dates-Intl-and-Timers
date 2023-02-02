50d9220d0cbac0315d35acfc45f5e3188e648b18
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-02-02 16:48:02 -0700

    Timers: setTimeout and setInterval

--- script.js
+++ script.js
@@ -282,14 +282,20 @@ btnLoan.addEventListener("click", function (e) {
     amount > 0 &&
     currentAccount.movements.some((mov) => mov >= amount * 0.1)
   ) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(function () {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString());
+      // Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
 
-    // Update UI
-    updateUI(currentAccount);
+      // Update UI
+      updateUI(currentAccount);
+
+      // Reset timer
+      clearInterval(timer);
+      timer = startLogOutTimer();
+    }, 2500);
   }
   inputLoanAmount.value = "";
 });
@@ -514,19 +520,38 @@ btnSort.addEventListener("click", function (e) {
 
 //* Internationalizing Numbers (Intl) *//
 
-const num = 3884764.23;
+// const num = 3884764.23;
 
-const options = {
-  style: "currency",
-  unit: "celsius",
-  currency: "EUR",
-  // useGrouping: false,
-};
+// const options = {
+//   style: "currency",
+//   unit: "celsius",
+//   currency: "EUR",
+//   // useGrouping: false,
+// };
 
-console.log("US:      ", new Intl.NumberFormat("en-US", options).format(num));
-console.log("Germany: ", new Intl.NumberFormat("de-DE", options).format(num));
-console.log("Syria:   ", new Intl.NumberFormat("ar-SY", options).format(num));
-console.log(
-  navigator.language,
-  new Intl.NumberFormat(navigator.language, options).format(num)
+// console.log("US:      ", new Intl.NumberFormat("en-US", options).format(num));
+// console.log("Germany: ", new Intl.NumberFormat("de-DE", options).format(num));
+// console.log("Syria:   ", new Intl.NumberFormat("ar-SY", options).format(num));
+// console.log(
+//   navigator.language,
+//   new Intl.NumberFormat(navigator.language, options).format(num)
+// );
+
+//* Timers: setTimeout and setInterval
+
+// setTimeout
+const ingredients = ["olives", "spinach"];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+  3000,
+  ...ingredients
 );
+console.log("Waiting...");
+
+if (ingredients.includes("spinach")) clearTimeout(pizzaTimer);
+
+// setInterval
+setInterval(function () {
+  const now = new Date();
+  //console.log(now);
+}, 1000);
